0e4c8be06a40be25cb2a3446a85ead1ce2fea38d
jojoshua-dwrk <joshuadanielimanuel@gmail.com>
2026-04-24 15:23:39 +0700

    Add files via upload

---
+++ datadiri.js
@@ -0,0 +1,12 @@
+var number = 15240372;
+var string = "Joshua Daniel Immanuel Matulatuwa";
+var number2 = 23;
+var array = ['main','belajar', 'nyanyi' , "main .belajar .dan nyanyi"];
+var object ={ nama: "Joshua Daniel Immanuel Matulatuwa ", umur:23};
+
+document.getElementById("number").innerHTML="NIM:" + number;
+document.getElementById("string").innerHTML="Nama:" + string;
+document.getElementById("number2").innerHTML="umur:" + number2;
+document.getElementById("array").innerHTML="hobi:" + array[3];
+document.getElementById("object").innerHTML="nama saya: " + object.nama
+ + "saya berusia:  " + number2;
